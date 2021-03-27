323e8ed38a82a76a45c3c91d21420500b1fac28b
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-27 10:07:11 -0500

    add photo for avatar

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -45,7 +45,7 @@ function Sidebar() {
   return (
     <Container>
       <Header>
-        <UserAvatar onClick={() => auth.signOut()} />
+        <UserAvatar src={user.photoURL} onClick={() => auth.signOut()} />
 
         <IconContainer>
           <IconButton>
@@ -66,7 +66,7 @@ function Sidebar() {
 
       {/* list of chat */}
       {chatsSnapshot?.docs.map((chat) => (
-        <Chat key={chat.id} id={chat.id} user={chat.data().users} />
+        <Chat key={chat.id} id={chat.id} users={chat.data().users} />
       ))}
     </Container>
   );
